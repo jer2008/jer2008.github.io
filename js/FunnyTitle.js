9f0304ba0194d66ab677c9a1b398e82b820b5153
jer2008 <jer233@qq.com>
2022-11-12 21:19:48 +0800

    Site updated: 2022-11-12 21:19:48

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -4,12 +4,12 @@
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/img/fluid.png");
-         document.title = 'ヽ(●-`Д´-)ノ你丑你就走！';
+         document.title = '你不爱我了吗？o(TヘTo)' + OriginTitle;
          clearTimeout(titleTime);
      }
      else {
          $('[rel="icon"]').attr('href', "/img/fluid.png");
-         document.title = 'ヾ(Ő∀Ő3)ノ你帅就回来！' + OriginTitle;
+         document.title = 'ヾ(Ő∀Ő3)ノ终于回来了' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
