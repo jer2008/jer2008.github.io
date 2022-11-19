0167828a68c4f69085a70212e1723ce811cdb35e
jer2008 <jer233@qq.com>
2022-11-19 09:46:49 +0800

    Site updated: 2022-11-19 09:46:48

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -1,5 +1,5 @@
-// live2d_path 参数建议使用绝对路径
-// const live2d_path = "https://fastly.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/";
+// 注意：live2d_path 参数应使用绝对路径
+// const live2d_path = "https://cdn.jsdelivr.net/gh/stevenjoezhang/live2d-widget@latest/";
 const live2d_path = "/live2d-widget/";
 
 // 封装异步加载资源的方法
@@ -31,15 +31,16 @@ if (screen.width >= 768) {
 		loadExternalResource(live2d_path + "live2d.min.js", "js"),
 		loadExternalResource(live2d_path + "waifu-tips.js", "js")
 	]).then(() => {
-		// 配置选项的具体用法见 README.md
 		initWidget({
 			waifuPath: live2d_path + "waifu-tips.json",
 			//apiPath: "https://live2d.fghrsh.net/api/",
-			cdnPath: "https://cdn.jsdelivr.net/gh/jer2008/live2d_api@master/",
-			tools: ["hitokoto", "asteroids", "switch-model", "switch-texture", "photo", "info", "quit"]
+			cdnPath: "https://npm.elemecdn.com/akilar-live2dapi@latest/"
 		});
 	});
 }
+// initWidget 第一个参数为 waifu-tips.json 的路径，第二个参数为 API 地址
+// API 后端可自行搭建，参考 https://github.com/fghrsh/live2d_api
+// 初始化看板娘会自动加载指定目录下的 waifu-tips.json
 
 console.log(`
   く__,.ヘヽ.        /  ,ー､ 〉
