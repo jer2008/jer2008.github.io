c5296ae522f613f4bc6a26043019cd758c882153
jer2008 <117557416+jer2008@users.noreply.github.com>
2022-12-12 14:41:36 +0000

    手机端悬浮小窗适应+live2d支持手机端 Updated By Github Actions

--- live2d-widget/autoload.js
+++ live2d-widget/autoload.js
@@ -25,7 +25,8 @@ function loadExternalResource(url, type) {
 }
 
 // 加载 waifu.css live2d.min.js waifu-tips.js
-if (screen.width >= 768) {
+// if (screen.width >= 768) {
+{
 	Promise.all([
 		loadExternalResource(live2d_path + "waifu.css", "css"),
 		loadExternalResource(live2d_path + "live2d.min.js", "js"),
